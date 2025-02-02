1acdccae47d6a9de2a7a6db2029c5f417b4dc2a3
Deepanshu Pal <dp5114@dseu.ac.in>
2025-02-02 20:21:58 +0530

    chore: comments removed

--- config/database.ts
+++ config/database.ts
@@ -6,11 +6,9 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-        // console.log(`process.env.MONGO_URI: ${process.env.MONGO_URI}`);
         const conn = await mongoose.connect(process.env.MONGO_URI!);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
         
-        // Initialize model if not exists
         FAQ.init();
   } catch (error) {
         console.error('Database connection error:', error);
